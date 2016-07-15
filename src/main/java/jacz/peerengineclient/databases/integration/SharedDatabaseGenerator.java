3874757906557736a527cf4aefa552a63677482d
alberto <alberto.anguita@gmail.com>
2016-07-15 11:03:44 +0200

    Shared database generator now correctly removes items that no longer exist in the integrated database

--- src/main/java/jacz/peerengineclient/databases/integration/SharedDatabaseGenerator.java
+++ src/main/java/jacz/peerengineclient/databases/integration/SharedDatabaseGenerator.java
@@ -10,10 +10,11 @@ import org.aanguita.jacuzzi.concurrency.timer.TimerAction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Handles the shared database, and its population with items from the integrated database
@@ -23,6 +24,71 @@ import java.util.concurrent.Executors;
  * be hard to track all this places, so we just do it automatically and periodically
  */
 public class SharedDatabaseGenerator implements TimerAction {
+
+    /**
+     * This class stores the relation of existing shared items, each time the shared database
+     * is to be updated. It allows finding elements that must be removed
+     */
+    private static class ExistingSharedItems {
+
+        private final Map<Integer, Movie> movies;
+
+        private final Map<Integer, TVSeries> tvSeries;
+
+        private final Map<Integer, Chapter> chapters;
+
+        private final Map<Integer, VideoFile> videoFiles;
+
+        private final Map<Integer, SubtitleFile> subtitleFiles;
+
+        public ExistingSharedItems(List<Movie> movies, List<TVSeries> tvSeries, List<Chapter> chapters, List<VideoFile> videoFiles, List<SubtitleFile> subtitleFiles) {
+            this.movies = buildItemMap(movies);
+            this.tvSeries = buildItemMap(tvSeries);
+            this.chapters = buildItemMap(chapters);
+            this.videoFiles = buildItemMap(videoFiles);
+            this.subtitleFiles = buildItemMap(subtitleFiles);
+        }
+
+        private static <T extends DatabaseItem> Map<Integer, T> buildItemMap(List<T> items) {
+            Map<Integer, T> itemMap = new HashMap<>();
+            for (T item : items) {
+                itemMap.put(item.getId(), item);
+            }
+            return itemMap;
+        }
+
+        public void checkMovie(Movie movie) {
+            movies.remove(movie.getId());
+        }
+
+        public void checkTVSeries(TVSeries tvSeries) {
+            movies.remove(tvSeries.getId());
+        }
+
+        public void checkChapter(Chapter chapter) {
+            movies.remove(chapter.getId());
+        }
+
+        public void checkVideoFile(VideoFile videoFile) {
+            movies.remove(videoFile.getId());
+        }
+
+        public void checkSubtitleFile(SubtitleFile subtitleFile) {
+            movies.remove(subtitleFile.getId());
+        }
+
+        public Collection<DatabaseItem> remainingItems() {
+            return Stream.concat(
+                    Stream.concat(
+                            Stream.concat(
+                                    Stream.concat(
+                                            movies.values().stream(), tvSeries.values().stream()
+                                    ), chapters.values().stream()
+                            ), videoFiles.values().stream()
+                    ), subtitleFiles.values().stream()
+            ).collect(Collectors.toList());
+        }
+    }
 
     private final static Logger logger = LoggerFactory.getLogger(SharedDatabaseGenerator.class);
 
@@ -43,6 +109,8 @@ public class SharedDatabaseGenerator implements TimerAction {
      * Before re-calculating the shared db, we must load this variable with the available hashes
      */
     private Set<String> availableHashes;
+
+    private ExistingSharedItems existingSharedItems;
 
     /**
      * Through the FileAPI, we can gather the files that we are currently downloading
@@ -87,32 +155,12 @@ public class SharedDatabaseGenerator implements TimerAction {
         // we use the sequential task executor so there cannot be concurrent processes
         sequentialTaskExecutor.submit(() -> {
             updateAvailableHashes();
+            resetExistingSharedItems();
             // go through all movies and series
             logger.info("Starting shared database generation...");
-            logger.info("listing movies in integrated...");
-            List<Movie> movies = Movie.getMovies(integratedPath);
-            logger.info("listed movies in integrated...");
-            for (Movie movie : movies) {
-                if (checkFiles(movie.getVideoFiles())) {
-                    // this movie must be included in the shared db
-                    logger.info("adding movie " + movie.getId() + " to shared...");
-                    addProducedCreationItem(movie);
-                } else {
-                    removeProducedCreationItem(movie);
-                }
-            }
-            logger.info("listing tvSeries in integrated...");
-            List<TVSeries> tvSeries = TVSeries.getTVSeries(integratedPath);
-            logger.info("listed tvSeries in integrated...");
-            for (TVSeries aTvSeries : tvSeries) {
-                if (checkChapters(aTvSeries.getChapters())) {
-                    // this tv series must be included in the shared db
-                    logger.info("adding tv series " + aTvSeries.getId() + " to shared...");
-                    addProducedCreationItem(aTvSeries);
-                } else {
-                    removeProducedCreationItem(aTvSeries);
-                }
-            }
+            checkMovies();
+            checkTVSeries();
+            removeRemainingSharedItems();
             logger.info("Shared database generation complete!");
         });
     }
@@ -121,9 +169,47 @@ public class SharedDatabaseGenerator implements TimerAction {
         availableHashes = fileAPI.getAvailableHashes();
     }
 
-    private boolean checkChapters(List<Chapter> chapters) {
+    private void resetExistingSharedItems() {
+        existingSharedItems = new ExistingSharedItems(
+                Movie.getMovies(sharedPath),
+                TVSeries.getTVSeries(sharedPath),
+                Chapter.getChapters(sharedPath),
+                VideoFile.getVideoFiles(sharedPath),
+                SubtitleFile.getSubtitleFiles(sharedPath));
+    }
+
+    private void checkMovies() {
+        List<Movie> movies = Movie.getMovies(integratedPath);
+        for (Movie movie : movies) {
+            existingSharedItems.checkMovie(movie);
+            if (checkFiles(movie.getVideoFiles())) {
+                // this movie must be included in the shared db
+                logger.info("adding movie " + movie.getId() + " to shared...");
+                addProducedCreationItem(movie);
+            } else {
+                removeProducedCreationItem(movie);
+            }
+        }
+    }
+
+    private void checkTVSeries() {
+        List<TVSeries> tvSeries = TVSeries.getTVSeries(integratedPath);
+        for (TVSeries aTvSeries : tvSeries) {
+            existingSharedItems.checkTVSeries(aTvSeries);
+            if (checkChapters(aTvSeries)) {
+                // this tv series must be included in the shared db
+                logger.info("adding tv series " + aTvSeries.getId() + " to shared...");
+                addProducedCreationItem(aTvSeries);
+            } else {
+                removeProducedCreationItem(aTvSeries);
+            }
+        }
+    }
+
+    private boolean checkChapters(TVSeries aTvSeries) {
         boolean anyTrue = false;
-        for (Chapter chapter : chapters) {
+        for (Chapter chapter : aTvSeries.getChapters()) {
+            existingSharedItems.checkChapter(chapter);
             if (checkFiles(chapter.getVideoFiles())) {
                 // this chapter must be included in the shared db
                 logger.info("adding chapter " + chapter.getId() + " to shared...");
@@ -153,7 +239,10 @@ public class SharedDatabaseGenerator implements TimerAction {
             // before anything else, check if we must add or remove subtitle files (if add, we need to do it before
             // adding the actual video file, so this is the best place to do it)
             VideoFile videoFile = (VideoFile) file;
+            existingSharedItems.checkVideoFile(videoFile);
             checkFiles(videoFile.getSubtitleFiles());
+        } else {
+            existingSharedItems.checkSubtitleFile((SubtitleFile) file);
         }
         String hash = file.getHash();
         boolean isAdded;
@@ -245,6 +334,18 @@ public class SharedDatabaseGenerator implements TimerAction {
                     sharedItem.delete();
                     integratedToShared.remove(integratedItem.getItemType(), integratedItem.getId());
                 }
+            } finally {
+                concurrencyController.endActivity(IntegrationConcurrencyController.Activity.INTEGRATED_TO_SHARED.name());
+            }
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
+    private void removeRemainingSharedItems() {
+        if (concurrencyController.beginActivity(IntegrationConcurrencyController.Activity.INTEGRATED_TO_SHARED.name())) {
+            try {
+                existingSharedItems.remainingItems().stream().forEach(DatabaseItem::delete);
             } finally {
                 concurrencyController.endActivity(IntegrationConcurrencyController.Activity.INTEGRATED_TO_SHARED.name());
             }
